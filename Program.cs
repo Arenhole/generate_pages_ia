95dc6b7a0feb14d18b3dd29d2db4e4eafe531e74
Samuel Cottarel <55884478+SamuelCottarel@users.noreply.github.com>
2023-09-13 11:16:10 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+class Program
+{
+    static async Task Main()
+    {
+        using HttpClient client = new();
+        try
+        {
+            string url = "https://jsonplaceholder.typicode.com/users";
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonContent = await response.Content.ReadAsStringAsync();
+                var users = JsonConvert.DeserializeObject<HttpClientDemo.User[]>(jsonContent);
+
+                Console.WriteLine("Liste des utilisateurs :");
+                if (users is not null)
+                {
+                    foreach (var user in users)
+                    {
+                        Console.WriteLine(user.Name);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Erreur lors de la récupération des données : " + response.StatusCode);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Une erreur s'est produite : " + ex.Message);
+        }
+    }
+}
+
---
+++ test/HttpUser.cs
@@ -0,0 +1,13 @@
+namespace HttpClientDemo
+{
+    public class User
+    {
+
+        public User()
+        {
+            Name = string.Empty;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
